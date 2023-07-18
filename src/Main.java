88fdf4b8897985aed104a97d008cf656fc1b7b1e
KalinaVina <dariya.kalvina@gmail.com>
2023-07-18 10:35:45 +0300

    h

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main  {
     public static void main(String[] args){
         System.out.println("Hi, dude");
         System.out.println("Have a nice day");
+        System.out.println("12");
     }
 }
